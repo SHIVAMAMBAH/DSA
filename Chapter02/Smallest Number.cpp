6f6d97f6dc28e314c4ef4b97301b0f62e0c5e58a
SDiHALF <shivamsharma22468@gmail.com>
2024-09-25 01:16:15 +0530

    Update Smallest Number.cpp

--- Chapter02/Smallest Number.cpp
+++ Chapter02/Smallest Number.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
 
+int min(int a, int b){
+  if(a>b){
+    return b;
+  }else{
+    return a;
+  }
+}
+
+int SmallesNumber(int arr[], int size){
+  int smallest = INT_MAX;
+
+  for(int i = 0;i<size;i++){
+    smallest = min(smallest,arr[i]);
+  }
+  return smallest;
+}
+
+int main()
+{
+  int arr[] = {21,34,21,11,14};
+
+  int size = sizeof(arr)/sizeof(int);
+
+  std::cout<<SmallestNumber(arr,size);
+
+  return 0;
+}
