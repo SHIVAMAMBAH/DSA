de0e32ad169d13b71eb59c1cc362b9d4b989d5af
SDiHALF <shivamsharma22468@gmail.com>
2024-09-25 01:59:37 +0530

    Update Linear Search.cpp

--- Chapter02/Searching an Array/Linear Search.cpp
+++ Chapter02/Searching an Array/Linear Search.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
 
+int LinearSearch(int arr[],int size, int target)
+{
+    for(int i = 0;i<size;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[5 ] = {21,23,43,45,54};
+    
+    std::cout<<LinearSearch(arr,5,213);
+    
+    return 0;
+}
