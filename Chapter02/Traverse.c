4f1eafc18e0a0ecae7719b18359eb1243cb27b73
SDiHALF <shivamsharma22468@gmail.com>
2024-09-25 01:26:14 +0530

    Update Traverse.c

--- Chapter02/Traverse.c
+++ Chapter02/Traverse.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+//  both are correct
+
+// traversing using the array ine=dexing
+void Traverse(int arr[],int size)
+{
+  for(int i = 0;i<size;i++){
+    printf("%d\n",arr[i]);
+  }
+}
+
+// traversing using the pointers
+// void Traverse(int arr[],int size)
+// {
+//   for(int i = 0;i<size;i++){
+//     printf("%d\n",*(arr+i));
+//   }
+// }
+
+int main()
+{
+  int arr[] = {21,34,54,27,89}; 
+
+  // both are correct
+  int size = sizeof(arr)/sizeof(int);
+  // int size = sizeof(arr)/sizeof(arr[0]);
+
+  Traverse(arr,size);
+  return 0;
+}
