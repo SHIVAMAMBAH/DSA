2e99750dfc0428694ea19ded689b09fc28184a66
SDiHALF <shivamsharma22468@gmail.com>
2024-09-25 01:29:26 +0530

    Update Largest Number.c

--- Chapter02/Largest Number.c
+++ Chapter02/Largest Number.c
@@ -1,1 +1,31 @@
 
+#include<stdio.h>
+#include<limits.h>
+
+int max(int a, int b){
+  if(a>b){
+    return a;
+  }else{
+    return b;
+  }
+}
+
+int LargestNumber(int arr[], int size){
+  int largest = INT_MAX;
+
+  for(int i = 0;i<size;i++){
+    smallest = max(smallest,arr[i]);
+  }
+  return largest;
+}
+
+int main()
+{
+  int arr[] = {21,34,21,11,14};
+
+  int size = sizeof(arr)/sizeof(int);
+
+  printf("%d",LargestNumber(arr,size));
+
+  return 0;
+}
