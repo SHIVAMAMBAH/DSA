89c6a115056c6277568e9ac6d4d2c6bb071bd17d
SDiHALF <shivamsharma22468@gmail.com>
2024-09-25 02:01:04 +0530

    Update Linear Search.c

--- Chapter02/Searching an Array/Linear Search.c
+++ Chapter02/Searching an Array/Linear Search.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int LinearSearch(int arr[],int size, int target)
+{
+    for(int i = 0;i<size;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[5 ] = {21,23,43,45,54};
+    
+    printf("%d\n",LinearSearch(arr,5,23));
+    printf("%d\n",LinearSearch(arr,5,129));
+    
+    return 0;
+}
